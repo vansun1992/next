5e3cac7bba7f30b3a188b7f8c36c21a86489b3c5
vansun1992 <56790653+vansun1992@users.noreply.github.com>
2021-03-06 14:24:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'TOP-8f4e25a0a54542379e88e52571e025a4',
+  rootNotionPageId: '2241f2ddc09f452cbbd63a830e32439e,
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'TOP-8f4e25a0a54542379e88e52571e025a4',
+  rootNotionSpaceId: null,
 
   // basic site info (required)
   name: 'gooood.top',
